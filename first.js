cec7fa7da1eb7cdf90a7dc121fd5ca366d73a7ae
Shefat bapari <shefatbapary01@gmail.com>
2024-01-26 09:22:18 -0500

    module almost done

--- first.js
+++ first.js
@@ -12,4 +12,90 @@ var areYouSingle = true;
 var youWhoIam = false;
 
 
+
+
 //                          class 6 Operatorsop
+
+// ADDTIONS
+// var eggPrice = 10;
+// var potatoPrice = 20.5;
+// var totalPrice = (eggPrice + potatoPrice + .50 -1)
+// console.log(eggPrice + potatoPrice, totalPrice);
+
+
+
+// var eggPrice = 10;
+// var potatoPrice = 20.5;
+// var priceDifference = (potatoPrice - eggPrice)
+// console.log(priceDifference);
+
+// var applePrice = 10;
+// var quantity = 10;
+// var totalPrice = (applePrice * quantity)
+// console.log(totalPrice);
+
+
+
+var money = 1000;
+var prayer = 10;
+var parPlayerMoney = (money / prayer)
+console.log(parPlayerMoney);
+
+
+//  class 7
+
+var potatoPrice = 50;
+// variable update
+potatoPrice = 45;
+
+// add something to the previous value
+
+var price1 = 44;
+var price2 = 10;
+
+price1 = price1 + 1;
+
+// shorthand
+price2 += 10;
+console.log(price1, price2)
+price2++;
+console.log(price2)
+
+
+
+// class 8 
+
+var firstName = "Shefat";
+var lastname  = "bapari";
+var fullName  = firstName + lastname;
+console.log(fullName)
+//  need to use word gap 
+fullName = firstName + " " + lastname;
+console.log(fullName)
+
+
+
+// class 8 .... 2
+
+var number = "10";
+var number2 = "20";
+console.log(number + number2)
+
+// ba 1ta string taklay o tara 1 shata cholay asa
+var num = 10 ;
+var num2= "20";
+console.log(num + num2)
+
+// convert string to number 
+ num2 = parseInt(num2);
+ console.log(num + num2)
+
+//  .ola number k String to number a convart korar 
+
+var gpa = '4.40';
+console.log(parseFloat(gpa))
+let kesoLekboNa;
+
+// class 9 
+
+console.log(typeof true,typeof 44, typeof"44", typeof kesoLekboNa)
